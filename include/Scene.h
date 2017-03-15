314cf110e30426caaf76a4682b703b25f369b1c8
Hedi Nasr <h.nsr69@gmail.com>
2017-03-15 21:49:51 +0100

    reformat code

--- include/Scene.h
+++ include/Scene.h
@@ -1,41 +1,36 @@
-
 #ifndef _SCENE_H
 #define _SCENE_H
 
 #include "Animation.h"
 #include "Image.h"
 
-//! The structure Scene stores all texture Id or images used by the sceneDraw fonction
-struct Scene
-{
-    Animation anim;
-	unsigned int tex_papillon;
+//! The structure Scene stores all texture Id or images used by the sceneDraw
+//! fonction
+struct Scene {
+  Animation anim;
+  Image terrain;
 
-	Image terrain;
-	unsigned int tex_terrain;
-    
-    unsigned int tex_arbre;
-    
-    unsigned int tex_eau;
- 
-    unsigned int tex_cube;
+  unsigned int tex_terrain;
+  unsigned int tex_papillon;
+  unsigned int tex_arbre;
+  unsigned int tex_eau;
+  unsigned int tex_cube;
 };
 
+//! Dessine les arbres
 void draw_arbre();
-//dessine les arbres
 
 //! Dessine un terrain
-void terraindraw(const Image & im, const Scene & sc);
+void terraindraw(const Image &im, const Scene &sc);
 
 //! Dessine les normales en chaque points du terrain
-void terrainNormal(const Image & im, const int i, const int j);
+void terrainNormal(const Image &im, const int i, const int j);
 
 //! Init a Scene
-void sceneInit(Scene& sc);
+void sceneInit(Scene &sc);
 
 //! Draw the scene
-void sceneDraw(const Scene& sc);
-
+void sceneDraw(const Scene &sc);
 
 //! Draw a cube
 void draw_cube();
@@ -57,6 +52,5 @@ void draw_grid();
 
 //! Dessine le dessin que je veux =)
 void dessine_();
-
 
 #endif
